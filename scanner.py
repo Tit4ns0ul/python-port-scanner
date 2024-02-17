dad3c2875caf20e6dde58f8c50a4ba0b07cf180b
Kamal Singh Bisht <workwithkamal@outlook.com>
2024-02-17 14:39:16 +0530

    Update_scanner_tool.py
    Simple Port Scanner
    
    This Python script is a simple port scanner that allows users to scan a target host for open TCP ports within a specified range. By providing an IP address or hostname as a command-line argument, users can initiate a scan to identify open ports on the target system

--- scanner.py
+++ scanner.py
@@ -4,37 +4,37 @@ import sys
 import socket 
 from datetime import datetime 
 
-#Define our target 
-if len(sys.argv) ==2: #scanner.py <ip>
-	target = socket.gethostbyname(sys.argv[1]) #translate hostname to IPv4
+# Define our target 
+if len(sys.argv) == 2:  # Check if the correct number of arguments is provided
+    target = socket.gethostbyname(sys.argv[1])  # Translate hostname to IPv4
 else: 
-	print("invalid amount of arguements.")
-	print("syntax: python3 scanner.py <ip>" )
+    print("Invalid number of arguments.")
+    print("Syntax: python3 scanner.py <ip>")
 
-#adding a pretty banner
-print("-" *50)
-print("scanning target : " + target)
-print("time started " + str(datetime.now()))
-print("-" *50 )
+# Adding a pretty banner
+print("-" * 50)
+print("Scanning target: " + target)
+print("Time started: " + str(datetime.now()))
+print("-" * 50)
 
 try: 
-	for port in range(50,85):
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		socket.setdefaulttimeout(1)
-		result = s.connect_ex((target,port))
-		if result == 0:
-			print(f"port {port} is open'")
-		s.close()
-		
+    # Iterate through ports and attempt to connect
+    for port in range(50, 85):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target, port))
+        if result == 0:
+            print(f"Port {port} is open'")
+        s.close()
+        
 except KeyboardInterrupt:
-	print("\n Exiting program !") 
-	sys.exit()
-	
+    print("\nExiting program!") 
+    sys.exit()
+    
 except socket.gaierror:
-	print(" Hostname could not be resolved ! ") 
-	sys.exit() 
-	
+    print("Hostname could not be resolved!") 
+    sys.exit() 
+    
 except socket.error:
-	print("could not connect to server.") 
-	sys.exit()	
-					
+    print("Could not connect to server.") 
+    sys.exit()
